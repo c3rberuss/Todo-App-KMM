25ad093608bb87cb43490c835903f67e2680d441
Josué Amaya <josue@ese.plus>
2022-11-23 16:22:47 -0600

    refactor: Id field was added to Task model

--- shared/src/commonMain/kotlin/xyz/c3rberuss/todoapp/shared/app/domain/Task.kt
+++ shared/src/commonMain/kotlin/xyz/c3rberuss/todoapp/shared/app/domain/Task.kt
@@ -1,6 +1,7 @@
 package xyz.c3rberuss.todoapp.shared.app.domain
 
 data class Task(
+    val id: Int,
     val name: String,
     val description: String,
     val status: TaskStatus,
--- shared/src/commonMain/kotlin/xyz/c3rberuss/todoapp/shared/database/Mappers.kt
+++ shared/src/commonMain/kotlin/xyz/c3rberuss/todoapp/shared/database/Mappers.kt
@@ -3,6 +3,7 @@ package xyz.c3rberuss.todoapp.shared.database
 import xyz.c3rberuss.todoapp.shared.app.domain.Task
 
 internal fun TaskEntity.toDomain() = Task(
+    id = id.toInt(),
     name = name,
     description = description,
     status = status,
